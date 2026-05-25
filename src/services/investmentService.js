5c88a03bf94988d85a82d85f77d3fe69a7c3e5ba
ezzzni <unin5412@gmail.com>
2026-05-25 15:03:32 +0900

    feat: DELETE /investments/:id 구현

--- src/services/investmentService.js
+++ src/services/investmentService.js
@@ -37,3 +37,23 @@ export async function updateInvestment(id, { amount, comment, password }) {
     updatedAt: updatedData.updatedAt,
   };
 }
+
+export async function removeInvestment(id, { password }) {
+  const investment = await prisma.virtualInvestment.findUnique({
+    where: { id },
+  });
+
+  if (!investment) {
+    throw new NotFoundError("해당 투자를 찾을 수 없습니다.");
+  }
+
+  if (investment.password !== password) {
+    throw new ValidationError("비밀번호가 일치하지 않습니다.");
+  }
+
+  await prisma.virtualInvestment.delete({
+    where: { id },
+  });
+
+  return;
+}
